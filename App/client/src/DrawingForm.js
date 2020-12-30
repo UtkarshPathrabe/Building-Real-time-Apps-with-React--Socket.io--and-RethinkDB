d8fe72ebad7494f13c89ff916abbe4804e91f345
Utkarsh Ashok Pathrabe <UtkarshPathrabe@users.noreply.github.com>
2020-12-30 14:28:37 +0530

    Managing Real Time List of Drawings

---
+++ App/client/src/DrawingForm.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { createDrawing } from './api';
+
+class DrawingForm extends Component {
+	state = {
+		drawingName: '',
+	};
+
+	handleDrawingNameChange = (event) => {
+		this.setState({
+			drawingName: event.target.value,
+		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		createDrawing(this.state.drawingName);
+		this.setState({ drawingName: '' });
+	};
+
+	render() {
+		return (
+			<div className='Form'>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type='text'
+						value={this.state.drawingName}
+						onChange={this.handleDrawingNameChange}
+						placeholder='Drawing name'
+						className='Form-drawingInput'
+						required
+					/>
+					<button type='submit' className='Form-button'>
+						Create
+					</button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default DrawingForm;
